2471b17b4bf72fc19bc51208ec5e35ae443657b4
imaa77 <amisuresh7@gmail.com>
2026-03-23 10:40:45 +0530

    Updated DB connection for InfinityFree

--- connection.php
+++ connection.php
@@ -2,19 +2,18 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 
-$servername = "127.0.0.1";
-$username   = "root";
-$password   = "";
-$database   = "medvault";
-$port       = 3307;
+$servername = "sql207.infinityfree.com";
+$username   = "if0_41454102";
+$password   = "Medvault20003";
+$database   = "if0_41454102_medvault";
 
-$con = new mysqli($servername, $username, $password, $database, $port);
+$con = new mysqli($servername, $username, $password, $database);
 
 if ($con->connect_error) {
     header("Content-Type: application/json");
     echo json_encode([
         "success" => false,
-        "message" => "Database connection failed"
+        "message" => "Database connection failed: " . $con->connect_error
     ]);
     exit;
 }
